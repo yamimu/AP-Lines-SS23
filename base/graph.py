56a2bc915668f5dd197f11f820a5ede248198561
Jan Müller <jan.mueller@stud.uni-heidelberg.de>
2023-05-29 22:29:06 +0200

    added documentaion to class Node;Graph

--- base/graph.py
+++ base/graph.py
@@ -3,21 +3,31 @@ import numpy as np
 # shortest path
 
 class Node:
-    #ndim vector 
-    def __init__(self, coord = None, adjacent_Nodes = None) -> None:
-        self.adjacent_Nodes = []
+    """
+    Represents a single node in a Graph, does not contain any
+    knowledge about other nodes
+    """
+    def __init__(self, coord = None) -> None:
+        """
+        Initalizes a Node with n Dimensional Coordinates
+        @param self
+        @param coord (default = [0,0])
+        """
         if coord != None:
             if type(coord) != np.array:
                 coord = np.array(coord)
             self.coord = coord
-            if adjacent_Nodes != None:
-                self.adjacent_Nodes = [node for node in adjacent_Nodes if node.dim() == self.dim()]
         else:
             self.coord = [0,0]
 
-    def calculate_distance_to(self,node):
+    def euclidian_distance(self,node):
         ''' 
-        
+        calculates the euclidian distance between self and
+        another node, when the dimensions match
+        @param self
+        @param node
+
+        returns distance as float
         '''
         if self.dim() != node.dim():
             raise(ValueError("dim don't match"))
@@ -27,9 +37,21 @@ class Node:
        return len(self.coord)
     
 
+#TODO be able to add and remove nodes 
 class Graph:
+    """
+    This class represents weighted graphs using adjacency matrices
+    """
     def __init__(self,nodes, edges = None, undirected = True) -> None:
-
+        """
+        @param self
+        @param nodes of the graph
+        @param edges (default None) 
+            can either be type(Node) or indices of nodes
+        @param undirected (default = False)
+            bool specifying if graph is directed 
+            --> graph directed
+        """
         self.n_nodes = len(nodes)
         self.undirected = undirected
         self.nodes = nodes
@@ -38,13 +60,24 @@ class Graph:
         if edges != None:
             for edge in edges:
                 i,j = self.check_edge(edge)
-                w = self.nodes[i].calculate_distance_to(self.nodes[j])
+                w = self.nodes[i].euclidian_distance(self.nodes[j])
                 #add weights here
                 if undirected:
                     self.adjacency_matrix[i][j] = w
                 self.adjacency_matrix[j][i] = w
 
     def to_index(self,node):
+        """
+        returns the index of a node in this graph to be able to use
+        indices when working with edges instead of always looking up
+        edges
+        @param self
+        @param node 
+            node which index to be returned
+
+        return:
+            index of node if node is part of the graph
+        """
         if type(node) != Node:
             raise(ValueError("can only convert Nodes to indices"))
         if node not in self.nodes:
@@ -52,28 +85,46 @@ class Graph:
         return self.nodes.index(node)
 
     def check_edge(self,edge):
-
+        """
+        performs type checks on a given edge to prevent invalid inputs
+        @param self
+        @param edge
+        return:
+            i,j index of first and second node in edge
+        """
         if type(edge[0]) != type(edge[1]):
             raise(ValueError("edge type missmatch"))
         if type(edge[0]) not in [Node, int]:
             raise(ValueError("edge must contain either nodes or indices"))
-        if type(edge[0]) == Node :
+        if isinstance(edge[0],Node) :
             i,j = self.to_index(edge[0]), self.to_index(edge[1])
-        if type(edge[0]) == int :
+        if isinstance(edge[0], int) :
             i,j = edge[0], edge[1]
         return i,j
     
     
-    def add_edge(self,edge):
+    def add_edge(self,edge, w = None):
+        """
+        adds edge to graph automatically calculating the euclidian distance
+        as the weight respects directionality
 
+        @param self
+        @param edge edge to be added
+        @param w (default = None) weight to be put in the adjacency_matix
+        """
         i,j = self.check_edge(edge)
-        w = self.nodes[i].calculate_distance_to(self.nodes[j])
+        if isinstance(w, None):
+            w = self.nodes[i].euclidian_distance(self.nodes[j])
         self.adjacency_matrix[i][j] = w
         if self.undirected:
             self.adjacency_matrix[j][i] = w
 
     def remove_edge(self,edge):
-
+        """
+        removes edge from graph respects directionality
+        @param self
+        @param edge
+        """
         i,j = self.check_edge(edge)
         self.adjacency_matrix[i][j] = 0
         if self.undirected:
