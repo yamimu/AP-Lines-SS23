d497df41cbb87ebe82fafc446816f3db45a8eeee
Timo Ege <Timoege@online.de>
2023-06-18 14:32:40 +0200

    completed function set_start_point and raising Error if point is not near graph.

--- lines/base/game_functions.py
+++ lines/base/game_functions.py
@@ -1,9 +1,5 @@
 from graph import Graph, Node
-from graph_base_functions import check_point_on_graph
-
-
-
-
+from graph_base_functions import find_point_on_graph
 
 
 def set_start_point(x:int ,y:int, g: Graph):
@@ -16,15 +12,28 @@ def set_start_point(x:int ,y:int, g: Graph):
         :raises: None
         """
     start = Node((x,y))
-
-    graph_nodes = check_point_on_graph(g,start)
+    try:
+        graph_node = find_point_on_graph(g,start)
+    except ValueError as er:
+        print(er)
+        return None
     
-    if graph_nodes != None:
-        if len(graph_nodes) > 1:
-            g.remove_edge((graph_nodes[0],graph_nodes[1]))   
-            g.add_node(start,[graph_nodes[0],graph_nodes[1]])
-            g.add_start_node(start)
-        if len(graph_nodes) == 1:
-            g.add_start_node(graph_nodes[0])
+    
+    if graph_node[0] in g.nodes:
+        return g
+    else:
+        new_g = Graph(g.nodes,g.edge_list)
+        new_g.remove_edge(graph_node[1])
+        new_g.add_node(graph_node[0],graph_node[1])
+        return new_g
+    
          
-            
+         
+'''node0 = Node(coord = [0,0])
+node1 = Node(coord = [0,4])
+node2 = Node(coord = [4,4])
+node3 = Node(coord = [4,0])
+node4 = Node(coord = [2,8])
+edges = [(0,1),(0,3),(0,2),(1,2),(2,3)]
+g = Graph([node0,node1,node2,node3], edges)
+nod= set_start_point(node4.coord[0],node4.coord[1],g)'''   
