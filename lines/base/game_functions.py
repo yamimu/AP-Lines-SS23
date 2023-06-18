a9c0fb146b983391640a62bfafd90d45b6f26805
Jan Müller <jan.mueller@stud.uni-heidelberg.de>
2023-06-18 21:04:26 +0200

    started working on animation

--- lines/base/game_functions.py
+++ lines/base/game_functions.py
@@ -30,30 +30,53 @@ def set_start_point(x:int ,y:int, g: Graph):
          
             
 
-def next_step(g, start_index, step_length):
+def inital_step(g :Graph, start_index: int, step_length: float): 
     neighbour_indices = g.adjacency_matrix[start_index].nonzero()[0]
 
     neighbour_coord_list = np.array([n.coord for n in np.array(g.nodes)[neighbour_indices]])
     runners_coord_list = np.array([g.nodes[start_index].coord.T for i in range(len(neighbour_indices))])
 
     vs = (neighbour_coord_list - runners_coord_list)
-    us = vs / np.linalg.norm(vs,axis = 1)
+    norm_vs = np.linalg.norm(vs,axis = 1)
+    us = vs / norm_vs[:,None]
+    #print(f"rcl: {runners_coord_list}")
     runners_coord_list = runners_coord_list + step_length * us
+    #print(f"vs: {vs},\n norm_vs : {norm_vs},\n us : {us} \n")
+    #print(f"rcl: {runners_coord_list}") 
     ng = Graph([g.nodes[start_index]])
-    for entry in runners_coord_list:
+    runner_info = []
+    for entry, neigh_index in zip(runners_coord_list, neighbour_indices):
         ng.add_node(Node(entry),[0])
+        runner_info.append((ng.n_nodes-1,neigh_index))
     
-    return ng
+    
+    return ng, runner_info
     #coord_bp = step_length / \
     #            * na.coord + best_node.coord
 
-
+def next_step(og, g, runner_info, step_length: float):
+    runner_info_coord_list = np.array([[g.nodes[i].coord, og.nodes[j].coord] for i,j in runner_info])
+    #print(f"ricl: {runner_info_coord_list}")
+    vs = runner_info_coord_list[:,1] - runner_info_coord_list[:,0]
+    #print(f"vs : {vs}") 
+    norm_vs = np.linalg.norm(vs,axis = 1)
+    #print(f"norm_vs : {norm_vs}")
+    us = vs/ norm_vs[:,None]
+    #print(f"us : {us}")
+    runner_new_coord = runner_info_coord_list[:,0] + step_length * us
+    signs = np.sign(runner_info_coord_list[:,1] - runner_new_coord)
+    print(f"signs : {signs}")
+    print(f"rc : {runner_info_coord_list[:,0]}")
+    print(f"rnc : {runner_new_coord}")
+    #us = 
 
 
 if __name__ == "__main__":
-    g = Graph([Node([0,0]),Node([1,0]), Node([2,0])],[(0,1),(1,2)])
-    g = next_step(g, 1, 0.2).toNx()
+    og = Graph([Node([-1,0]),Node([0,.2]), Node([2,0])],[(0,1),(1,2)])
+    ng, runner_info = inital_step(og, 1, 0.5)
+    next_step(og,ng,runner_info, 1)
+    g = ng.toNx()
     nx.draw_networkx(g, nx.get_node_attributes(g, 'pos'), labels=nx.get_node_attributes(g, 'label'))
-    plt.show()
+    #plt.show()
 
     
